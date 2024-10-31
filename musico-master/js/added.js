70a079e8eaa521702de313f4a5f1feba0ca1c744
Seum <26SeumP@mydacc.org>
2024-10-31 10:50:46 -0400

    10/31/2024 10:50 AM

--- musico-master/js/added.js
+++ musico-master/js/added.js
@@ -4,3 +4,19 @@ btn.addEventListener("click", function(){
     const myInsertText = "thanks for your purchace";
     myText.innerHTML = myInsertText;
 });
+
+
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init({
+      duration: 1200,   // Set the animation duration in ms
+      once: true,       // Whether animation should happen only once
+      easing: 'ease-in-out', // Animation easing
+    });
+  });
+  
