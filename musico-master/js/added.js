31542ae34b45afe59ea22d3cea0eafd4b7b680e8
Seum <26SeumP@mydacc.org>
2024-11-04 10:44:49 -0500

    11/4/2024 10:44 AM

--- musico-master/js/added.js
+++ musico-master/js/added.js
@@ -12,11 +12,5 @@ import 'aos/dist/aos.css';
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    AOS.init({
-      duration: 1200,   // Set the animation duration in ms
-      once: true,       // Whether animation should happen only once
-      easing: 'ease-in-out', // Animation easing
-    });
-  });
+
   
