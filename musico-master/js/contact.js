ff71a71c9c6a2e5cda62f039e97c5b23b25ad4f6
Seum <26SeumP@mydacc.org>
2024-12-06 10:43:24 -0500

    12/6/2024 10:43 AM

--- musico-master/js/contact.js
+++ musico-master/js/contact.js
@@ -35,23 +35,22 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
+                    required: "Please enter your name.",
                     minlength: "your name must consist of at least 2 characters"
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
+                    required: "Please enter your subject here.",
                     minlength: "your subject must consist of at least 4 characters"
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
+                    required: "Please enter your number here.",
                     minlength: "your Number must consist of at least 5 characters"
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Please enter a valid email address."
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Please enter your message here.",
                 }
             },
             submitHandler: function(form) {
